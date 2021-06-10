7d706cd692a3b10b78797ba3b4feab5ba12ffb15
Rosalyn Land <rosalyn.land@digitalworkroom.co.uk>
2021-06-10 14:56:55 +0100

    Initial Commit

---
+++ Tinder For Furniture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tinder For Furniture
+//
+//  Created by Rosalyn Land on 10/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tinder For Furniture/Tinder_For_FurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_For_FurnitureApp.swift
+//  Tinder For Furniture
+//
+//  Created by Rosalyn Land on 10/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_For_FurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
