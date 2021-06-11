3dbf641865b0a86052bb19d6af66d992413a96b3
Tom Hogg <tom.hogg@digitalworkroom.co.uk>
2021-06-11 11:40:10 +0100

    implemented firebase in project

--- Tinder For Furniture/FavouritesStore.swift
+++ Tinder For Furniture/FavouritesStore.swift
@@ -11,7 +11,7 @@ class FavouritesStore: ObservableObject {
     @Published var favourites: [Furniture] = []
     
     func toggleFaves(furniture: Furniture) {
-        furniture.isLiked.toggle()
+//        furniture.isLiked.toggle()
     }
     
     
